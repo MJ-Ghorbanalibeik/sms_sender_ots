6fbfa9f23343690812e87c648bd17bb2ed93ca33
Mojtaba <mojtaba.ghorbanalibeik@gmail.com>
2015-05-20 21:23:27 +0430

    update tests

---
+++ test/sms_sender_ots_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class SmsSenderOtsTest < ActiveSupport::TestCase
+  test "truth" do
+    assert_kind_of Module, SmsSenderOts
+  end
+
+  test "complete_cycle" do
+    balance_before = SmsSenderOts.get_balance(ENV['userid'], ENV['password'])
+    assert_equal balance_before[:error], nil
+    send_sms_result = SmsSenderOts.send_sms(ENV['userid'], ENV['password'], ENV['mobile_number'], ENV['sender'], 'This message has been sent from automated test')
+    assert_not_equal send_sms_result[:message_id], nil
+    assert_equal send_sms_result[:error], nil
+    balance_after = SmsSenderOts.get_balance(ENV['userid'], ENV['password'])
+    assert_equal balance_after[:error], nil
+    assert_equal balance_before[:balance], balance_after[:balance] + 1
+    # Query message
+    query_result = SmsSenderOts.query_message(ENV['userid'], ENV['password'], send_sms_result[:message_id])
+    assert_equal query_result[:error], nil
+    assert_equal query_result[:code], 5
+  end
+end
