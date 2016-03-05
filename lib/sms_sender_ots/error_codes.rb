f0fb66ca3e697cb810e6475694074bc325014152
Mojtaba <mojtaba.ghorbanalibeik@gmail.com>
2016-03-05 15:57:24 +0330

    stub requests using webmock
    add circleci config
    
    remove unnecessary database gem
    
    add webmock to circle config
    
    add expected way of installing bundle
    
    yet another config
    
    yet another config
    
    move dotenv gem out of test group
    
    add dotenv to dependencies of circleci
    
    update tests to iterate through all defined messages
    
    import enviroment variables
    
    add utf-8 encoding to test file
    
    include sender_id if it exists in webmock
    
    fix erorr codes
    
    yet another fix
    
    and yet another fix
    
    why do you persist on asking me for commit messages
    
    it is realy getting annoying
    
    allright it was a typo, but even that is not a good reason to keep asking me for commit messages
    
    not giving up huh
    
    maybe i am the crazy one after all! who pushes another commit after everything is fixed

--- lib/sms_sender_ots/error_codes.rb
+++ lib/sms_sender_ots/error_codes.rb
@@ -12,7 +12,7 @@ module SmsSenderOts
         when "ER-04"
           {error: "Wrong sender ID format, sender ID should not exceed 11 characters or 16 numbers, only English letters allowed with no special characters or spaces", code: 4}
         when "ER-05"
-          {error: "Sender ID is blocked and can’t be used", code: 5}
+          {error: "Sender ID is blocked and can't be used", code: 5}
         when "ER-06"
           {error: "Sender ID doesn't exist", code: 6}
         when "ER-07"
@@ -48,9 +48,9 @@ module SmsSenderOts
         when "ER-22"
           {error: "Language not supported", code: 22}
         when "ER-23"
-          {error: "You have exceeded your Sender ID’s requests limit, delete one sender ID to request a new one", code: 23}
+          {error: "You have exceeded your Sender ID's requests limit, delete one sender ID to request a new one", code: 23}
         when "ER-24"
-          {error: "Wrong Status data , message status should be one of the following statuses “Queued” , “Sent” , “Failed” or “Rejected”", code: 24}
+          {error: "Wrong Status data , message status should be one of the following statuses \"Queued\" , \"Sent\" , \"Failed\" or \"Rejected\"", code: 24}
         when "ER-25"
           {error: "This request is not included in your account plan", code: 25}
         when "ER-26"
@@ -70,7 +70,7 @@ module SmsSenderOts
         when "ER-33"
           {error: "Wrong channel type, Channel value should be TextMessage, Call or Both", code: 33}
         when "ER-34"
-          {error: "Wrong time to live value TTL, TTL should be between 1 – 60 minutes, and should not exceed the Expiry time", code: 34}
+          {error: "Wrong time to live value TTL, TTL should be between 1 and 60 minutes, and should not exceed the Expiry time", code: 34}
         when "ER-35"
           {error: "MessageID already sent", code: 35}
         when "ER-36"
--- test/sms_sender_ots_test.rb
+++ test/sms_sender_ots_test.rb
@@ -1,22 +1,48 @@
+# encoding: utf-8
 require 'test_helper'
 
 class SmsSenderOtsTest < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, SmsSenderOts
+  test_messages = [
+    SmsSenderOts::MobileNumberNormalizer.normalize_message('This message has been sent from automated test 😎'),
+    SmsSenderOts::MobileNumberNormalizer.normalize_message('این پیام از آزمون خودکار فرستاده شده است 😎')
+  ]
+  # Config webmock for sending messages 
+  test_messages.each do |m|
+    request_body_header = {:body => {'AppSid' => ENV['appsid'], 'Recipient' => ENV['mobile_number'], 'Body' => m}, :headers => {'Content-Type'=>'application/x-www-form-urlencoded'}}
+    request_body_header[:body]['SenderID'] = ENV['sender'] if !ENV['sender'].blank?
+    WebMock::API.stub_request(:post, 'api.unifonic.com/rest/Messages/Send').
+      with(request_body_header).
+      to_return(:status => 200, 
+        :body => "{\"data\":{\"MessageID\":\"6542\",\"Status\":\"Sent\",\"NumberOfUnits\":\"2\",\"Cost\":0.4,\"Balance\":\"100\",\"Recipient\":\"#{ENV['mobile_number']}\",\"DateCreated\":\"2014-07-22\"}}", 
+        :headers => {'Content-Type' => 'application/json'})
   end
+  # Config webmock for query balance 
+  WebMock::API.stub_request(:post, 'api.unifonic.com/rest/Account/GetBalance').
+    with(:body => {'AppSid' => ENV['appsid']},
+      :headers => {'Content-Type'=>'application/x-www-form-urlencoded'}).
+    to_return(:status => 200, 
+      :body => "{\"success\":\"true\",\"message\":\"\",\"errorCode\":\"ER-00\",\"data\":{\"Balance\":\"48.03200\",\"CurrencyCode\":\"USD\",\"SharedBalance\":\"0.00000\"}}", 
+      :headers => {'Content-Type' => 'application/json'})
+  # Config webmock for query message
+  WebMock::API.stub_request(:post, 'api.unifonic.com/rest/Messages/GetMessageIDStatus').
+    with(:body => {'AppSid' => ENV['appsid'], 'MessageID' => '6542'},
+      :headers => {'Content-Type'=>'application/x-www-form-urlencoded'}).
+    to_return(:status => 200, 
+      :body => "{\"success\":\"true\",\"message\":\"\",\"errorCode\":\"ER-00\",\"data\":{\"Status\":\"Sent\",\"DLR\":\"Delivered\"}}", 
+      :headers => {'Content-Type' => 'application/json'})
 
   test "complete_cycle" do
     balance_before = SmsSenderOts.get_balance({password: ENV['appsid']})
     assert_equal balance_before[:error], nil
-    send_sms_result = SmsSenderOts.send_sms({password: ENV['appsid']}, ENV['mobile_number'], 'This message has been sent from automated test 😎', ENV['sender'])
-    assert_not_equal send_sms_result[:message_id], nil
-    assert_equal send_sms_result[:error], nil
+    test_messages.each do |m|
+      send_sms_result = SmsSenderOts.send_sms({password: ENV['appsid']}, ENV['mobile_number'], m, ENV['sender'])
+      assert_not_equal send_sms_result[:message_id], nil
+      assert_equal send_sms_result[:error], nil
+      query_result = SmsSenderOts.query_message({password: ENV['appsid']}, send_sms_result[:message_id])
+      assert_equal query_result[:error], nil
+      assert_equal query_result[:code], 0
+    end
     balance_after = SmsSenderOts.get_balance({password: ENV['appsid']})
     assert_equal balance_after[:error], nil
-    #assert_equal balance_before[:balance], balance_after[:balance] + 1
-    # Query message
-    query_result = SmsSenderOts.query_message({password: ENV['appsid']}, send_sms_result[:message_id])
-    assert_equal query_result[:error], nil
-    assert_equal query_result[:code], 0
   end
 end
